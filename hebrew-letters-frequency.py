0bdd5d3fbdc6bd943f59babc60991997ff7457d0
Erez Arbell <erez@arbell.co.il>
2019-10-13 18:24:13 +0300

    change to python3

--- hebrew-letters-frequency.py
+++ hebrew-letters-frequency.py
@@ -1,37 +1,33 @@
 # -*- coding: utf-8 -*-
-import urllib2
+import requests
 import sys
 
 def get_text(url):
-	response = urllib2.urlopen(url)
-	return response.read()
+    response = requests.get(url)
+    return response.text
 
 hebrew_letters = u"אבגדהוזחטיכלמנסעפצקרשת"
 final_letters = {
-	u'ך': u'כ',
-	u'ם': u'מ',
-	u'ן': u'נ',
-	u'ף': u'פ',
-	u'ץ': u'צ'
+    u'ך': u'כ',
+    u'ם': u'מ',
+    u'ן': u'נ',
+    u'ף': u'פ',
+    u'ץ': u'צ'
 }
-
-letters = dict()
-for letter in hebrew_letters:
-	letters[letter] = 0
+letters = { letter: 0 for letter in hebrew_letters }
 
 for url in sys.argv[1:]:
-	text = get_text(url)
-	text = unicode(text, "utf-8")
-	for letter in text:
-		if letter in final_letters.keys():
-			letter = final_letters[letter]
-		if letter in hebrew_letters:
-			letters[letter] += 1
+    text = get_text(url)
+    for letter in text:
+        if letter in final_letters.keys():
+            letter = final_letters[letter]
+        if letter in hebrew_letters:
+            letters[letter] += 1
 
 sum = 0
 for letter in letters:
-	sum += letters[letter]
+    sum += letters[letter]
 
-letters_tupple = [(letter,letters[letter]) for letter in hebrew_letters]
-for letter, c in sorted(letters_tupple, key=lambda (a,b): b):
-    print("%3s%8d%8.2f%%" % (letter , letters[letter], 100.0*letters[letter]/sum))
+letters_tupple = [(letter, letters[letter]) for letter in letters]
+for letter, count in sorted(letters_tupple, key=lambda a: a[1]):
+    print("%3s%8d%8.2f%%" % (letter, count, 100.0*count/sum))
